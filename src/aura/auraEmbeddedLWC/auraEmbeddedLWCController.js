86e4857065ac303e1c7bf54703e7dbdede8b7335
Saravanan Durairaj <saravanan.durairaj@crossmark.com>
2020-07-22 09:37:48 -0500

    aura

--- src/aura/auraEmbeddedLWC/auraEmbeddedLWCController.js
+++ src/aura/auraEmbeddedLWC/auraEmbeddedLWCController.js
@@ -1,8 +1,5 @@
 ({
     handleContactSelect: function (component, event) {
-        var service = component.find('service');
-
-        component.set('v.contactId', event.getParam('contactId'));
-        service.reloadRecord();
+       
     }
 });
